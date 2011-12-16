a9566b51df9547b5da0a90771566f61c1bcba5ce
Daniel Kuehn <daniel@kuehn.se>
2011-12-16 14:51:49 +0100

    Commented out the debug print statements and added a very basic check to parse_config to see if there is too many/few options in the config

--- monologue.py
+++ monologue.py
@@ -18,15 +18,24 @@ def parse_config(conf_file = "monologue.conf"):
                 config[line.split('=')[0].rstrip().lstrip()] = int(line.split('=')[1].rstrip().lstrip())
             else:
                 config[line.split('=')[0].rstrip().lstrip()] = line.split('=')[1].rstrip().lstrip()
-
-    for key, value in config.iteritems():
-        print ("%s %s = %s %s" % (type(key), key, type(value), value))
+    
+    if len(config) < 8:
+        print "You have too few options in the config."
+        exit(1)
+    elif len(config) > 9:
+        print "You have too many options in the config."
+        exit(1)
+    else:
+        print "Config looks alright, let's continue"
+                
+    #for key, value in config.iteritems():
+    #    print ("%s %s = %s %s" % (type(key), key, type(value), value))
 
 def privmsg(irc,data):
     user_info, msg_type, channel, message = irc.privmsg_split(data)
     username, real_user, host = irc.user_split(user_info)
     current_time = time()
-    print "%s %s %s %s" % (type(username), username, type(msg_counter['current_talker']), msg_counter['current_talker'])
+    #print "%s %s %s %s" % (type(username), username, type(msg_counter['current_talker']), msg_counter['current_talker'])
 
     if username != msg_counter['current_talker']:
         msg_counter['current_talker'] = username
@@ -37,7 +46,7 @@ def privmsg(irc,data):
         diff = float(current_time) - float(msg_counter['old_time'])
         if diff < 2.6:
             msg_counter['flood'] += 1
-        print "%s %s = %d flood: %d time taken: %s %f" % (msg_counter['current_talker'], channel.lstrip('#'), msg_counter[channel.lstrip('#')], msg_counter['flood'], type(diff), diff)
+        #print "%s %s = %d flood: %d time taken: %s %f" % (msg_counter['current_talker'], channel.lstrip('#'), msg_counter[channel.lstrip('#')], msg_counter['flood'], type(diff), diff)
     
     if msg_counter['flood'] > int(config['flood_limit']):
         irc.kick(channel, msg_counter['current_talker'], config['flood_kick_msg'])
@@ -53,8 +62,7 @@ def privmsg(irc,data):
 def sources(irc, data):
     user_info, msg_type, channel, message = irc.privmsg_split(data)
     username, real_user, host = irc.user_split(user_info)
-    irc.privmsg(channel, "%s: Sources for monologue can be found at: %s" % (username, "https://github.com/lejonet/Monologue"))
-    irc.privmsg(channel, "%s: Sources for aidsbot can be found at: %s" % (username, "https://github.com/adisbladis/aidsbot"))
+    irc.privmsg(channel, "%s: Sources for monologue can be found at: %s and sources for aidsbot can be found at: %s" % (username, "https://github.com/lejonet/Monologue", "https://github.com/adisbladis/aidsbot"))
 
 
 parse_config()
@@ -62,7 +70,7 @@ irc = aidsbot(config['botname'], config['server'], int(config['port']), True) #S
 irc.connect() #Actually connect
 for channel in config['channels']:
     irc.join(channel) #Join a channel
-    irc.privmsg(channel, 'Yo maddafakas!') #Send a message
+    irc.privmsg(channel, 'Sir, Ready for duty, Sir!') #Send a message
 
 irc.chanophandler_add("PRIVMSG", privmsg)
 irc.privmsghandler_add("!source", sources)
